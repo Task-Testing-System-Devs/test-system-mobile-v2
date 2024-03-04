85269fef23008b8d22225c859143830047806774
Evgeny <startsev2000@outlook.com>
2024-03-04 12:46:05 +0300

    Add test widgets to contest and submission pages

--- lib/src/features/contests/view/contests_page.dart
+++ lib/src/features/contests/view/contests_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:test_system/src/shared/enums/programming_language.dart';
+import 'package:test_system/src/shared/widgets/complexity_widget.dart';
+import 'package:test_system/src/shared/widgets/contest_widget.dart';
 
 class ContestsPage extends StatelessWidget {
   const ContestsPage({super.key});
@@ -6,8 +9,27 @@ class ContestsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Страница контестов'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                for (final complexity in Complexity.values)
+                  for (final lang in ProgrammingLanguage.values)
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: ContestWidget(
+                        name: 'Test contest',
+                        endDate: DateTime.now(),
+                        complexity: complexity,
+                        languages: [lang],
+                      ),
+                    ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/src/features/submissions/view/submissions_page.dart
+++ lib/src/features/submissions/view/submissions_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:test_system/src/features/submissions/model/submission.dart';
+import 'package:test_system/src/shared/widgets/submission_widget.dart';
+
+import '../../../shared/enums/programming_language.dart';
 
 class SubmissionsPage extends StatelessWidget {
   const SubmissionsPage({super.key});
@@ -6,8 +10,31 @@ class SubmissionsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Страница посылок'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                for (int i = 0; i < ProgrammingLanguage.values.length; ++i)
+                  for (final language in ProgrammingLanguage.values)
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: SubmissionWidget(
+                        submission: Submission(
+                          id: '${i + 1}',
+                          code: 'a = int(input())',
+                          language: language,
+                          status: 'OK',
+                          usedTime: 2.5,
+                          usedMemory: 5.4,
+                        ),
+                      ),
+                    ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
