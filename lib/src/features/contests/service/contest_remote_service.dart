c4f53641caa21e3ac26997a2c1dd959b367f2d91
Evgeny <startsev2000@outlook.com>
2024-03-10 17:29:47 +0300

    Add methods to contest service

--- lib/src/features/contests/service/contest_remote_service.dart
+++ lib/src/features/contests/service/contest_remote_service.dart
@@ -1,3 +1,78 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:test_system/src/features/contests/models/contest.dart';
 import 'package:test_system/src/features/contests/service/contest_service.dart';
+import 'package:test_system/src/features/submissions/model/submission.dart';
+import 'package:test_system/src/features/tasks/model/task.dart';
 
-class ContestRemoteService extends ContestService {}
+class ContestRemoteService extends ContestService {
+  final Dio _dio;
+
+  ContestRemoteService(this._dio);
+
+  static const _ejudgePath = '/api/master';
+
+  @override
+  Future<Contest> getContest(String contestId) async {
+    try {
+      final response = await _dio.get(
+        '$_ejudgePath/contest-status-json',
+        queryParameters: {
+          'contest_id': contestId,
+        },
+      );
+      return Contest.fromJson(response.data as Map<String, dynamic>);
+    } on DioException catch (e) {
+      debugPrint('${e.response?.requestOptions.path} $e');
+      rethrow;
+    }
+  }
+
+  @override
+  Future<Submission> getSubmission(
+    String contestId,
+    String submissionId,
+  ) async {
+    try {
+      final response = await _dio.get(
+        '$_ejudgePath/run-status-json',
+        queryParameters: {
+          'contest_id': contestId,
+        },
+      );
+      return Submission.fromJson(response.data as Map<String, dynamic>);
+    } on DioException catch (e) {
+      debugPrint('${e.response?.requestOptions.path} $e');
+      rethrow;
+    }
+  }
+
+  @override
+  Future<Task> getTask(String contestId, String taskId) async {
+    try {
+      final response = await _dio.get(
+        '$_ejudgePath/problem-statement-json',
+        queryParameters: {
+          'contest_id': contestId,
+          'problem': taskId,
+        },
+      );
+      return Task.fromJson(response.data as Map<String, dynamic>);
+    } on DioException catch (e) {
+      debugPrint('${e.response?.requestOptions.path} $e');
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> sendTaskSolution() async {
+    try {
+      await _dio.post(
+        '$_ejudgePath/submit-run',
+      );
+    } on DioException catch (e) {
+      debugPrint('${e.response?.requestOptions.path} $e');
+      rethrow;
+    }
+  }
+}
--- lib/src/features/contests/service/contest_service.dart
+++ lib/src/features/contests/service/contest_service.dart
@@ -1,3 +1,13 @@
-abstract class ContestService {
+import 'package:test_system/src/features/contests/models/contest.dart';
+import 'package:test_system/src/features/submissions/model/submission.dart';
+import 'package:test_system/src/features/tasks/model/task.dart';
 
+abstract class ContestService {
+  Future<Contest> getContest(String contestId);
+
+  Future<Task> getTask(String contestId, String taskId);
+
+  Future<void> sendTaskSolution();
+
+  Future<Submission> getSubmission(String contestId, String submissionId);
 }
